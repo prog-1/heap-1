423e911386982d97eb976412284f06260c6471da
Richard Shashalevich <richardsasalevic@gmail.com>
2022-12-11 01:52:49 +0200

    pop implementation

--- heap.go
+++ heap.go
@@ -18,10 +18,55 @@ func parent(index int) int {
 	return (index - 1) / 2
 }
 
+func left(index int) int {
+	return index*2 + 1
+}
+
+func right(index int) int {
+	return index*2 + 2
+}
+
 // Pop removes and returns the minimum element (according to Less) from the heap.
 // The complexity is O(log n) where n = h.Len().
 func (h *Heap) Pop() int {
-	return 0
+	tmp := (*h)[0]
+	(*h)[0] = (*h)[len(*h)-1]
+	(*h) = (*h)[:len(*h)-1]
+	var current int
+	for {
+		left := left(current)
+		if left+1 < len(*h) {
+			l, r := (*h)[left], (*h)[left+1]
+			if l < r {
+				if l < (*h)[current] {
+					(*h)[current], (*h)[left] = (*h)[left], (*h)[current]
+					current = left + 1
+				} else if r < (*h)[current] {
+					(*h)[current], (*h)[left+1] = (*h)[left+1], (*h)[current]
+					current = left + 2
+				} else {
+					break
+				}
+			} else {
+				if r < (*h)[current] {
+					(*h)[current], (*h)[left+1] = (*h)[left+1], (*h)[current]
+					current = left + 2
+				} else {
+					break
+				}
+
+			}
+
+		} else if left < len(*h) {
+			if (*h)[current] > (*h)[left] {
+				(*h)[current], (*h)[left] = (*h)[left], (*h)[current]
+			}
+			break
+		} else {
+			break
+		}
+	}
+	return tmp
 }
 
 func main() {
@@ -33,5 +78,17 @@ func main() {
 	h.Push(5)
 	h.Push(-1)
 
+	fmt.Println(h)
+	fmt.Println(h.Pop())
+	fmt.Println(h)
+	fmt.Println(h.Pop())
+	fmt.Println(h)
+	fmt.Println(h.Pop())
+	fmt.Println(h)
+	fmt.Println(h.Pop())
+	fmt.Println(h)
+	fmt.Println(h.Pop())
+	fmt.Println(h)
+	fmt.Println(h.Pop())
 	fmt.Println(h)
 }
