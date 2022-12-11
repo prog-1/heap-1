613793dca74f28e5dd0c789e5f214e6926061501
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2022-12-11 09:42:56 +0200

    Got rid of redundant casting in Push()

--- main.go
+++ main.go
@@ -16,15 +16,13 @@ func (h *Heap) Push(x int) {
 	// Add new element to the end of the Heap
 	// compare new element's value with its parent
 	// if new element < parent -> swap them
-	*h = Heap(append([]int(*h), x))
-	i := len([]int(*h)) - 1
-	for ; i > 0 && []int(*h)[i] < []int(*h)[(i-1)/2]; i = (i - 1) / 2 {
-		[]int(*h)[i], []int(*h)[(i-1)/2] = []int(*h)[(i-1)/2], []int(*h)[i]
+	*h = Heap(append(*h, x))
+	for i := len(*h) - 1; i > 0 && (*h)[i] < (*h)[(i-1)/2]; i = (i - 1) / 2 {
+		(*h)[i], (*h)[(i-1)/2] = (*h)[(i-1)/2], (*h)[i]
 	}
 }
 
 // Pop removes and returns the minimum element (according to Less) from the heap.
 // The complexity is O(log n) where n = h.Len().
 func (h *Heap) Pop() int {
-
 }
