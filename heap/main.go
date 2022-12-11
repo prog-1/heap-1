4dd78e23bd2d7086ff6f0525c92f5119ffa7b560
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2022-12-11 17:14:52 +0200

    push && pop done

---
+++ heap/main.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+type Heap []int
+
+func Parent(i int) int {
+	return (i - 1) / 2
+}
+
+func L(i int) int {
+	return 2*i + 1
+}
+func R(i int) int {
+	return 2*i + 2
+}
+
+func (h *Heap) Push(x int) {
+	i := len(*h) - 1
+	*h = append(*h, x)
+	for i > 0 && ((*h)[Parent(i)] > (*h)[i]) {
+		(*h)[Parent(i)], (*h)[i] = (*h)[i], (*h)[Parent(i)]
+		i = Parent(i)
+	}
+}
+
+func (h *Heap) Pop() int {
+	min := (*h)[0]
+	(*h)[0] = (*h)[len(*h)-1]
+	(*h) = (*h)[:len(*h)-1]
+	if len(*h) > 0 {
+		siftDown(0, h)
+	}
+	return min
+}
+
+func siftDown(i int, h *Heap) {
+	var min int
+	if R(i) >= len(*h) {
+		if L(i) >= len(*h) {
+			return
+		}
+		min = L(i)
+	} else {
+		if (*h)[L(i)] <= (*h)[R(i)] {
+			min = L(i)
+		}
+		min = R(i)
+	}
+	if (*h)[i] > (*h)[min] {
+		(*h)[i], (*h)[min] = (*h)[min], (*h)[i]
+		siftDown(min, h)
+	}
+}
+func main() {
+	var h Heap
+	var z Heap
+	h = append(h, 1, 5, 3, 8, 7, 4, 9, 13, 15)
+	h.Push(16)
+	z.Push(2)
+	fmt.Println(h, z)
+	minh := h.Pop()
+	fmt.Println(minh)
+	fmt.Println(h)
+	minz := z.Pop()
+	fmt.Println(minz)
+	fmt.Println(z)
+
+}
