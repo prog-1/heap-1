b4e8196a0577c14bbb562cf23d46d78623063bba
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2022-12-11 18:18:20 +0200

    half ik

--- heap/main.go
+++ heap/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
 type Heap []int
 
@@ -42,9 +47,9 @@ func siftDown(i int, h *Heap) {
 		}
 		min = L(i)
 	} else {
-		if (*h)[L(i)] <= (*h)[R(i)] {
-			min = L(i)
-		}
+		//if (*h)[L(i)] <= (*h)[R(i)] {
+		//	min = L(i)
+		//}
 		min = R(i)
 	}
 	if (*h)[i] > (*h)[min] {
@@ -52,12 +57,44 @@ func siftDown(i int, h *Heap) {
 		siftDown(min, h)
 	}
 }
+
+func ik(r io.Reader) (int, int) {
+	var n, m, days, minik int
+	fmt.Fscan(r, &n, &m)
+	var allEx []int
+	for i := 0; i < n; i++ {
+		fmt.Fscan(r, &allEx[i])
+	}
+	var dailyEx Heap
+
+	if m > len(allEx) {
+		m = len(allEx)
+	}
+
+	for _, i := range allEx {
+		if len(dailyEx) < m {
+			dailyEx.Push(allEx[i])
+		}
+		if len(dailyEx) == m {
+			days++
+			if minik <= dailyEx[0] {
+				minik = dailyEx[0]
+			} else {
+				minik++
+				dailyEx.Pop()
+			}
+		}
+	}
+	return days, minik
+}
+
 func main() {
 	var h Heap
 	var z Heap
 	h = append(h, 1, 5, 3, 8, 7, 4, 9, 13, 15)
 	h.Push(16)
 	z.Push(2)
+	z.Push(7)
 	fmt.Println(h, z)
 	minh := h.Pop()
 	fmt.Println(minh)
@@ -65,5 +102,7 @@ func main() {
 	minz := z.Pop()
 	fmt.Println(minz)
 	fmt.Println(z)
-
+	r := bufio.NewReader(os.Stdout)
+	d, ik := ik(r)
+	fmt.Println(d, ik)
 }
